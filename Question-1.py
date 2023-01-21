96d82e3f10494cf7c817535b7ae780f04083116a
Ramesh Sengamalai <rsengamalai@gmail.com>
2023-01-21 17:38:23 +0800

    Adding Scipy interperter package and question set

---
+++ Question-1.py
@@ -0,0 +1,26 @@
+# 1. What will be displayed?
+list1 = ['a', 'b', 'c', 'd']
+print(list1[-1])
+# Answer: d
+
+# 2. Whats wrong in the below for loop?
+list1 = ['a','b','c','d']
+for data in list1:
+    print(data)
+# Answer:
+# a
+# b
+# c
+# d
+
+# 3. Is the below if condition code proper?
+list1=['a','b','c','d']
+if list1['a']==null:
+    print("Found")
+# Answer: TypeError: list indices must be integers or slices, not str
+
+# 4. What will get displayed in the below code?
+x=100.3567
+y=int(x)
+print(y)
+# Answer: 100
